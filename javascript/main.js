c00cb27a3f6a13548becb4a7c80dece9a65a637e
Ta <mahsasarafraz2016@gmail.com>
2025-05-04 11:28:16 +0330

    myfinalp

---
+++ javascript/main.js
@@ -0,0 +1,106 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 12.5,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySlider", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myoffer", {
+  slidesPerView: 8.1,
+  spaceBetween: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mybrands", {
+  slidesPerView: 9.3,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myincrdibelday", {
+  slidesPerView: 2.4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myincredebale-category", {
+  slidesPerView: 10.5,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mybestsells", {
+  slidesPerView: 4,
+  spaceBetween: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myincrdibeoffer", {
+  slidesPerView: 7.4,
+  spaceBetween: 8,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myfinish-offer", {
+  slidesPerView: 7.02,
+  spaceBetween: 0.5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myorder-offer", {
+  slidesPerView: 7.02,
+  spaceBetween: 0.5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myname-of-city", {
+  slidesPerView: 5,
+  spaceBetween: 0.5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
